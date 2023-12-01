d7d2e753220a3daccae7bde75c0aa8f806874257
amaher <amaher112@su.edu>
2023-11-30 23:08:27 -0500

    created for loop, adjusted some vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = ["a", "b", "c",]
-var uppercase = ["A", "B", "C",]
-var numbers = [1, 2, 3,]
-var specialCharacters = ["!", "*", "<"]
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split('');
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = "!#$%&'()*+,.=?/\^|`~<>;-_:@[]{}".split('');
 
 // Write password to the #password input
 function writePassword() {
@@ -18,15 +18,20 @@ function generatePassword() {
   var newPassword = []
   var characterBank = []
   var passwordLength = parseInt(prompt("How many characters do you want your password to be?"));
+ 
+  // Logic that prompts user to choose a certain number of characters
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please choose between 8 and 128 characters.")
     return
   } 
+
+  // Prompts to the user
   var hasLowercase = confirm("Would you like to use lowercase letters?");
   var hasUppercase = confirm("Would you like to use uppercase letters?");
   var hasNumbers = confirm("Would you like to use numbers?");
   var hasSpecialCharacters = confirm("Would you like to use special characters?");
   
+  // Logic dependent on user responses
   if (hasLowercase) {
     characterBank = characterBank.concat(lowercase);
   }
@@ -39,12 +44,17 @@ function generatePassword() {
   if (hasSpecialCharacters) {
     characterBank = characterBank.concat(specialCharacters);
   }
-  
-  
-  return newPassword.join("")
 
+  // Logic creating the random password
+for (var i = 0; i < passwordLength; i++){
+  var randomIndex = Math.floor((Math.random() * passwordLength.length -1) +1);
+  // var randomIndex = characterBank[Math.floor(Math.random() * passwordLength.length)];
+console.log(randomIndex)
+};
+ 
+  // return newPassword.join("");
+ 
 }
-
 
 
 
